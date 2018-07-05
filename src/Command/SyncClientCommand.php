95890d8638d3023ef798ebf864aaa4cba951890d
Desdi <andreigarciacuadra@gmail.com>
2018-07-05 14:02:59 +0200

    initial commit

---
+++ src/Command/SyncClientCommand.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\Command;
+
+use React\Socket\ConnectionInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SyncClientCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('scm:syncclient')
+            ->setDescription('Starts sync server.')
+            ->setHelp('Starts sync server on SCM central server.')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $loop = \React\EventLoop\Factory::create();
+        $connector = new \React\Socket\Connector($loop);
+        $connector
+            ->connect('127.0.0.1:8080')
+            ->then(
+                function (ConnectionInterface $conn) use($output)  {
+                    $output->write("CONECTADO");
+                    $conn->write("test");
+                    /*
+                     * Cerrar conexión.
+                     */
+                    $conn->close();
+                },
+                function (Exception $exception) use ($loop,$output){
+                    $output->write("Cannot connect to server: " . $exception->getMessage());
+                    $loop->stop();
+                });
+
+        $loop->run();
+    }
+}
---
+++ src/Command/SyncSrvCommand.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Command;
+
+use React\Socket\ConnectionInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SyncSrvCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('scm:syncsrv')
+            ->setDescription('Starts sync server.')
+            ->setHelp('Starts sync server on SCM central server.')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $loop = \React\EventLoop\Factory::create();
+        $socket = new \React\Socket\Server('127.0.0.1:8080', $loop);
+
+        $socket->on('connection', function(ConnectionInterface $connection) use($output) {
+            $output->write("CONECTADO CLIENTE");
+            $connection->write('Hi!');
+            $connection->on('data', function($data) use ($connection,$output){
+                $output->write("datos cliente:".$data);
+                $connection->write(strtoupper($data));
+            });
+        });
+
+        echo "Listening on {$socket->getAddress()}\n";
+
+        $loop->run();
+    }
+}
